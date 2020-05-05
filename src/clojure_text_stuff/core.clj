bcb76179c7d5209117a20fdaae035f5763a587cb
Jimmy Blundell <jblundell123@gmail.com>
2020-05-05 17:39:28 -0500

    Initial commit

---
+++ src/clojure_text_stuff/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-text-stuff.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(+ 1 1)
+
---
+++ test/clojure_text_stuff/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-text-stuff.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-text-stuff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
